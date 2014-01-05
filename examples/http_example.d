f2c5febbe0dc7b2733e4a101e699a56d95d8ff50
thepumpkin1979 <johan@heapsource.com>
2014-01-05 11:08:33 -0500

    [examples] use a new fiber per connection when accept http connections

--- examples/http_example.d
+++ examples/http_example.d
@@ -13,22 +13,24 @@ void main() {
         "listening http://localhost:3000".writeln;
         while(true) {
             HttpConnection connection = server.accept;
-            debug writeln("HTTP Agent just connected");
+            new core.thread.Fiber({
+                debug writeln("HTTP Agent just connected");
 
-            connection.process ^ (request, response) {
-                debug writeln("Processing ", request.method, request.rawUri, " as protocol version ", request.protocolVersion.toString);
-                foreach(h;request.headers) {
-                    debug writeln("Header ", h.name, h.value);
-                }
-                response.write("Hello World from heaploop\r\n");
-                response.write("something else\r\n");
-                response.end;
-                debug writeln("Ended");
-            };
+                connection.process ^ (request, response) {
+                    debug writeln("Processing ", request.method, request.rawUri, " as protocol version ", request.protocolVersion.toString);
+                    foreach(h;request.headers) {
+                        debug writeln("Header ", h.name, h.value);
+                    }
+                    response.write("Hello World from heaploop\r\n");
+                    response.write("something else\r\n");
+                    response.end;
+                    debug writeln("Ended");
+                };
+
+                debug writeln("continuing after process");
+            }).call;
             //core.memory.GC.collect;
             //core.memory.GC.minimize;
-
-            debug writeln("continuing after process");
         }
     };
     writeln("hello world");
